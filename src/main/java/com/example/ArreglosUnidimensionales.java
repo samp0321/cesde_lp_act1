2aae4fc286dfc2e3f6ee00ea766d9c39ca01edb6
Samuel Pulgarin <samuelpulgarinn1@gmail.com>
2026-05-23 12:42:22 -0500

    Avance 6

--- src/main/java/com/example/ArreglosUnidimensionales.java
+++ src/main/java/com/example/ArreglosUnidimensionales.java
@@ -2,10 +2,32 @@ package com.example;
 
 public class ArreglosUnidimensionales {
     public static void declararCrearInicializar() {
+
+    int numeros[] = {734, 384, 824, 234, 984, 123, 456, 789, 321, 654};
+    int numeroMenor = numeros[0];
+        for (int i = 1; i < numeros.length; i++) {
+            if (numeros[i] < numeroMenor) {
+                numeroMenor = numeros[i];
+            }
+        }
+        System.out.println("El número menor es: " + numeroMenor);
     }
 
     public static void mostrarLongitud() {
-        // TODO
+        String[]calendario = {
+        "Enero", 
+        "Febrero", 
+        "Marzo", 
+        "Abril", 
+        "Mayo", 
+        "Junio", 
+        "Julio", 
+        "Agosto", 
+        "Septiembre", 
+        "Octubre", 
+        "Noviembre", 
+        "Diciembre"};
+        System.out.println("El número de meses en el calendario es: " + calendario.length);
     }
 
     public static void recorrerConForClasico() {
@@ -35,7 +57,12 @@ public class ArreglosUnidimensionales {
     }
 
     public static int[] arrayComoParametroYRetorno(int[] array) {
-        // TODO
-        return array;
+        int edad = array.length;
+        int[] arrayP = new int[edad];
+        for (int i = 0; i < edad; i++) {
+            arrayP[edad - 1 - i] = i + 1;
+            System.out.println(arrayP[i]);
+        }
+        return arrayP;
     }
 }
--- src/main/java/com/example/MetodosEstaticosSimples.java
+++ src/main/java/com/example/MetodosEstaticosSimples.java
@@ -2,23 +2,44 @@ package com.example;
 
 public class MetodosEstaticosSimples {
     public static void ejemploDeclaracionLlamada() {
-        // TODO
-    }
+        System.out.println("======================================");
+        System.out.println("Declaración Llamada");
+        System.out.println("======================================");
+        System.out.println();
+        }
 
     public static void ejemploPasoParametros(String nombre) {
-        // TODO
+        System.out.println("======================================");
+        System.out.println("Paso de Parámetros");
+        System.out.println("======================================");
+        System.out.println("Hola, bienvenido " + nombre);
+        System.out.println();
     }
 
     public static int ejemploRetornoValores() {
-        // TODO
-        return 0;
+        int tres = 3;
+        int resultado = tres + tres;
+
+        System.out.println("======================================");
+        System.out.println("Retorno de Valores");
+        System.out.println("======================================");
+        System.out.println("Resultado: " + resultado);
+        System.out.println();
+
+        return resultado;
     }
 
     public static void ejemploSobrecarga() {
-        // TODO
+        System.out.println("======================================");
+        System.out.println("Sobrecarga sin parámetros");
+        System.out.println("======================================");
+        System.out.println();
     }
 
     public static void ejemploSobrecarga(int n) {
-        // TODO
+        System.out.println("======================================");
+        System.out.println("Sobrecarga con un parámetro: " + n);
+        System.out.println("======================================");
+        System.out.println();
     }
 }
